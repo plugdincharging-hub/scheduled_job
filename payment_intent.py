37faff31f786ee88a9f86a52cf3360dd51d72da8
Andrew14242414 <111254775+Andrew14242414@users.noreply.github.com>
2026-09-19 07:28:23 -0400

    Adding time ranged for scheduler

--- payment_intent.py
+++ payment_intent.py
@@ -1,12 +1,14 @@
 import requests as r
 import os
 import time
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone, time as dt_time
+from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def increment_payment(non_returns: list, headers:str):
+    responses = []
     for value in non_returns:
         post_intent_response = r.post(f"https://api.stripe.com/v1/payment_intents/{value}/increment_authorization",
             headers=headers,
@@ -18,7 +20,8 @@ def increment_payment(non_returns: list, headers:str):
         except r.exceptions.HTTPError:
             raise RuntimeError(post_intent_response.json())
         
-        return post_intent_response.json()
+        responses.append(post_intent_response.json())
+    return responses
 
 def filter_payments(json_response: list):
     non_returns = []
@@ -27,10 +30,9 @@ def filter_payments(json_response: list):
         if time.time() - value.get("created") > 16200 and value.get("status") == "requires_capture":
             non_returns.append(value.get("id"))
         else:
-            print(f"No values to increment, {value.get('id')}")
+            print(f"No value to increment, {value.get('id')}")
 
     return non_returns
-          
 
 def fetch_payments(headers: str, time_cut: dict):
         params = {
@@ -52,6 +54,20 @@ def fetch_payments(headers: str, time_cut: dict):
 
 
 def main():
+
+    uk_now = datetime.now(ZoneInfo("Europe/London"))
+
+    allowed = (
+        (uk_now.weekday() == 4 and uk_now.time() >= dt_time(19,0)) or
+        (uk_now.weekday() == 5) or
+        (uk_now.weekday() == 6) or
+        (uk_now.weekday() == 0 and uk_now.time() <= dt_time(2, 0))
+    )
+
+    if not allowed:
+        print("Outside of time range, exiting")
+        exit()
+
     one_day_ago = int(
             (datetime.now(timezone.utc) - timedelta(days=1)).timestamp())
     key = os.getenv("PROD_KEY")
